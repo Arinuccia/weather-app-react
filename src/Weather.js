6ad8e4af49e895463e6a07394d7b6a2149c1b50b
Arinuccia <yastremskaya.01@gmail.com>
2024-01-03 17:23:08 +0100

    added html, created a structure

--- src/Weather.js
+++ src/Weather.js
@@ -3,17 +3,74 @@ import axios from "axios";
 export default function Weather() {
   return (
     <div className="Weather">
-      <h1> Prague</h1>
+      <h1> Weather app</h1>
       <ul>
+        <li>City</li>
         <li>Time</li>
-        <li>Day</li>
       </ul>
       <div className="row">
-        <div className="col-6">6C</div>
-        <div className="col-6">
+        <div className="col-12">
+          <ul>
+            <li>6C</li>
+            <li>Affirmation</li>
+          </ul>
+        </div>
+
+        <div className="col-12">
           <ul>
             <li>Wind</li>
             <li>Humidity</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <button>Get an affirmation</button>
+      </div>
+      <div>
+        <button>Get a playlist for my weather</button>
+      </div>
+      <div className="row">
+        <div className="col-2">
+          <ul>
+            <li>Mon</li>
+            <li>Temp</li>
+            <li>img</li>
+            <li>Description</li>
+          </ul>
+        </div>
+
+        <div className="col-2">
+          <ul>
+            <li>Tue</li>
+            <li>Temp</li>
+            <li>img</li>
+            <li>Description</li>
+          </ul>
+        </div>
+
+        <div className="col-2">
+          <ul>
+            <li>Wed</li>
+            <li>Temp</li>
+            <li>img</li>
+            <li>Description</li>
+          </ul>
+        </div>
+
+        <div className="col-2">
+          <ul>
+            <li>Thu</li>
+            <li>Temp</li>
+            <li>img</li>
+            <li>Description</li>
+          </ul>
+        </div>
+        <div className="col-2">
+          <ul>
+            <li>Fri</li>
+            <li>Temp</li>
+            <li>img</li>
+            <li>Description</li>
           </ul>
         </div>
       </div>
